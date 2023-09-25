09334f5861fee565f6f039448ebb8d6057cca824
ajummer <makeudreamempty@gmail.com>
2023-09-25 12:26:06 +0300

    Trim not needed parts from views

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
-const path = require("path")
+const path = require("path");
 
 
 const app = express();
 const port = 3000; 
 
 // Handlebars config
-app.engine("hbs", handlebars.engine({extname: "hbs"}))
-app.set("view engine" , "hbs")
-app.set("views" , "src/views")
+app.engine("hbs", handlebars.engine({extname: "hbs"}));
+app.set("view engine" , "hbs");
+app.set("views" , "src/views");
 
 // Setup static files 
-app.use(express.static(path.resolve(__dirname,"public")))
+app.use(express.static(path.resolve(__dirname,"public")));
 
 app.get("/" , (req,res) =>{
     res.render("home")
 })
 
-app.listen(port , () => console.log(`Server is listening at port ${port}`))
+app.listen(port , () => console.log(`Server is listening at port ${port}`));
