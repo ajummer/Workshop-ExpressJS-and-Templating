584777db020f4dba1110608343a64f75429777a3
ajummer <makeudreamempty@gmail.com>
2023-09-25 12:43:16 +0300

    Fixed the path to static files

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 
 const expressConfig = (app) => {
-    app.use(express.static(path.resolve(__dirname,"public")));
+    app.use(express.static(path.resolve(__dirname,"../public")));
 }
 
-module.exports = expressConfig;
+module.exports = expressConfig; 
