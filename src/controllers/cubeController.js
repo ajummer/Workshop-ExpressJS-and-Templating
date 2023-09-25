d35af002de61a6c17236736f75f92b7b45245b32
ajummer <makeudreamempty@gmail.com>
2023-09-25 19:04:55 +0300

    Add body parser to express config

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
+const createCube = require("../services/cubeService.js")
 
 router.get("/create", (req, res) => {
   res.render("create");
 });
+
+router.post("/create" , (res,req) => {
+  console.log(req.body)
+})
 
 module.exports = router;
---
+++ src/services/cubeService.js
@@ -0,0 +1,25 @@
+const cubes = [
+  {
+    id: "5c39f1851ab3b24f5c03e182",
+    name: "Gan356 Air SM",
+    description: "Nice cube Bro",
+    imageUrl:
+      "https://ae01.alicdn.com/kf/HTB1CSddXRxRMKJjy0Fdq6yifFXa6/Gan-356-Air-SM-3x3-Black-Magic-cube-GAN-Air-SM-Magnetic-3x3x3-Speed-cube-gans.jpg",
+    difficultyLevel: 3,
+  },
+];
+const uniqid = require("uniqid");
+
+exports.createCube = (cubeData) => {
+  const newCube = {
+    id: uniqid(),
+    ...cubeData,
+  };
+
+  cubes.push(newCube);
+  return newCube;
+};
+
+exports.getAllCubes = () => {
+  return [...cubes];
+};
